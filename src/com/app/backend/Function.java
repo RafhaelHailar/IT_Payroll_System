802178d4347e3f0a5d8d52aaec9af53b52f99e6e
Rafhael <rafhael4hailar@gmail.com>
2024-01-29 02:39:26 +0800

    Compiled Success!

--- src/com/app/backend/Function.java
+++ src/com/app/backend/Function.java
@@ -1,9 +1,8 @@
 package com.app.backend;
 
-import java.util.ArrayList;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
+import java.util.ArrayList;
 
 public class Function extends DBConnection {
      // for pagination
@@ -12,25 +11,189 @@ public class Function extends DBConnection {
      private int positionIdEntered;
      public static boolean dataDisplaying = false;
      
-     // read api's
-     public void display() {
-         String query = "Select * FROM users";
+     // create api
+     public void createEmployee(String employeeName,String employeeSex,int employeeAge,int positionId) {
+         String query = "INSERT INTO employees (employee_name,employee_sex,employee_age,position_id) " 
+                + "VALUES (?, ?, ?, ?)";
          
          try {
              connect();
-             state = con.createStatement();
-             result = state.executeQuery(query);
              
-             while (result.next()) {
-                 int id = result.getInt("user_id");
-                 System.out.println(id);
-             }
+             prep = con.prepareStatement(query);
+             prep.setString(1, employeeName);
+             prep.setString(2, employeeSex);
+             prep.setInt(3, employeeAge);
+             prep.setInt(4, positionId);
+             
+             prep.executeUpdate();
              
              con.close();
          } catch (Exception e) {
              System.out.println(e);
          }
      }
+     
+     public boolean addEmployee(String employeeName, String employeeSex, int employeeAge, int positionId, String password) {
+ 	    String query = "INSERT INTO employees (employee_name, employee_sex, employee_age, position_id) VALUES (?, ?, ?, ?)";
+ 	    boolean isSuccess = false;
+ 	    	
+ 	    try {
+ 	        connect();
+ 	        prep = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+
+ 	        prep.setString(1, employeeName);
+ 	        prep.setString(2, employeeSex);
+ 	        prep.setInt(3, employeeAge);
+ 	        prep.setInt(4, positionId);
+ 	        prep.executeUpdate();
+
+ 	        // Retrieve the auto increment employeeId
+ 	        ResultSet generatedKeys = prep.getGeneratedKeys();
+ 	        if (generatedKeys.next()) {
+ 	            int generatedEmployeeId = generatedKeys.getInt(1);
+ 	            addEmployeeCredentials(generatedEmployeeId, password);
+ 	            addToAttendances(generatedEmployeeId);
+ 	        }
+
+ 	        con.close();
+ 	        System.out.println("\n------------------------------");
+ 	        System.out.println("Successfully added an employee");
+ 	        System.out.println("------------------------------\n");
+ 	        displayEmployeeBasicInfo(0);
+                
+                isSuccess = true;
+ 	    } catch (Exception e) {
+ 	        System.out.println("Connection Error" + e.getMessage());
+ 	    }
+            
+            return isSuccess;
+ 	}
+
+ 	
+ 	
+ 	// METHOD FOR ADDING EMPLOYEE CREDENTIALS
+ 	public void addEmployeeCredentials(int employeeId, String password) {
+ 	    String query = "INSERT INTO users (user_password, employee_id) VALUES (?, ?)";
+
+ 	    try {
+ 	        connect();
+ 	        prep = con.prepareStatement(query);
+ 	        prep.setString(1, password);
+ 	        prep.setInt(2, employeeId);
+ 	        prep.executeUpdate();
+ 	        con.close();
+ 	    } catch (Exception e) {
+ 	        e.printStackTrace();
+ 	    }
+ 	}
+ 	
+ 	
+ 	public void addToAttendances(int employeeId) {
+ 		 String attendanceQuery = "INSERT INTO attendances (employee_id) VALUES (?)";
+ 		 
+ 		 try {
+ 			 connect();
+ 			 prep = con.prepareStatement(attendanceQuery);
+ 			 prep.setInt(1, employeeId);
+ 			 prep.executeUpdate();
+ 			 con.close();
+ 		 }catch(Exception e) {
+ 			 System.out.println(e);
+ 		 }
+ 	}
+     
+     
+     // read api's
+        
+    //METHOD FOR CHECKING IF THE EMPLOYEE EXISTS IN OUR DATABASE
+    private boolean isEmployeeExists(int employeeId) {
+        String checkQuery = "SELECT COUNT(*) FROM employees WHERE employee_id = ?";
+        try {
+            connect();
+            prep = con.prepareStatement(checkQuery);
+            prep.setInt(1, employeeId);
+            ResultSet result = prep.executeQuery();
+            result.next();
+            int count = result.getInt(1);
+            con.close();
+            return count > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false; 
+        }
+    }
+    
+     public void displayEmployeePayroll() {
+        String query = "SELECT attendances.*, positions.position_pay_per_day, employees.employee_name "
+                + "FROM attendances "
+                + "INNER JOIN employees ON attendances.employee_id = employees.employee_id "
+                + "INNER JOIN positions ON positions.position_id = employees.position_id "
+                + "ORDER BY employee_id";
+
+        int netSalary = 0;
+        float grossSalary = 0;
+        int lateDeductions = 50;
+        System.out.println("********************************************************************");
+        System.out.println("*\t\t\t\t\t\t\t\t   *");
+        System.out.println("*\t\t     Payroll for the month of December\t           *");
+        System.out.println("*\t\t\t\t\t\t\t\t   *");
+        System.out.println("********************************************************************");
+        System.out.println("Employee ID\t  Employee Name\t\tNet Salary\tGross Salary");
+
+        try {
+            connect();
+            state = con.createStatement();
+            result = state.executeQuery(query);
+
+            while (result.next()) {
+                int noOfPresent = result.getInt("no_of_present");
+                int noOfAbsent = result.getInt("no_of_absent");
+                int noOfHoursLate = result.getInt("no_of_hours_late");
+                int payPerDay = result.getInt("position_pay_per_day");
+                int employeeId = result.getInt("employee_id");
+                String employeeName = result.getString("employee_name");
+
+                netSalary = payPerDay * noOfPresent;
+                grossSalary = netSalary - (noOfAbsent * payPerDay + (noOfHoursLate * lateDeductions));
+
+                System.out.printf("%-20d%-20s%-15d%-15.2f\n", employeeId, employeeName, netSalary, grossSalary);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+     
+     public void displayAllUnsuspendEmployeeInfo(int rowSpan) {
+        String query = "SELECT * FROM employees "
+                     + "INNER JOIN positions ON employees.position_id = positions.position_id "
+                     + "WHERE employee_is_suspended = 0 LIMIT " + (rowLimit * rowSpan) + ", " + rowLimit;
+
+        try {
+            connect();
+            state = con.createStatement();
+            result = state.executeQuery(query);
+
+            System.out.printf("%-15s%-25s%-10s%-5s%-20s\n",
+                    "EMPLOYEE ID", "Employee Name", "Gender", "Age", "Position Name");
+
+            while (result.next()) {
+                int employeeId = result.getInt("employee_id");
+                String employeeName = result.getString("employee_name");
+                String gender = result.getString("employee_sex");
+                int employeeAge = result.getInt("employee_age");
+                String positionName = result.getString("position_name");
+
+                System.out.printf("[%d]\t\t%-25s%-10s%-5d%-20s\n",
+                        employeeId, employeeName, gender, employeeAge, positionName);
+            }
+        } catch (Exception e) {
+            System.out.println("Connection Error: " + e.getMessage());
+        }
+        
+        funcId = 3;
+        dataDisplaying = true;
+    }
      
      public boolean login(int userId,String password) {
          String query = "SELECT * FROM users WHERE user_id = " + userId;
@@ -126,7 +289,7 @@ public class Function extends DBConnection {
      public void displayEmployeeByPosition(int positionId,int rowSpan) {
          String query = "SELECT * FROM employees INNER JOIN positions "
                  + "ON employees.position_id = positions.position_id WHERE employees.position_id = " + positionId
-                 + "LIMIT " + (rowLimit * rowSpan) + ", " + rowLimit;
+                 + " LIMIT " + (rowLimit * rowSpan) + ", " + rowLimit;
          
          try {
              connect();
@@ -155,14 +318,14 @@ public class Function extends DBConnection {
           
          // for pagination
          positionIdEntered = positionId;
-         funcId = 3;
+         funcId = 4;
          dataDisplaying = true;
      }
      
-     public void displayPositionsName() {
+     public int displayPositionsName() {
          String query = "SELECT position_id,position_name FROM positions";
          String display = "";
-         
+         int positionIdLimit = 0;
          try {
              connect();
              
@@ -174,6 +337,7 @@ public class Function extends DBConnection {
                  int id = result.getInt("position_id");
                  
                  System.out.printf("[%d]%s ", id, name);
+                 positionIdLimit++;
              }
              
              con.close();
@@ -182,28 +346,8 @@ public class Function extends DBConnection {
          }
          
          System.out.println(" "); // for spacing
-     }
-     
-     public void expi() {
-         String query = "SELECT * FROM employees LIMIT 5,5";
          
-         try {
-             connect();
-             
-             state = con.createStatement();
-             result = state.executeQuery(query);
-             //while (result.next()) {
-                 result.absolute(5);
-               //  String name = result.getString("employee_name");
-                 int row = result.getRow();
-                 System.out.println(row);
-               //  System.out.printf("%s index:  %d", name, row);
-           //  }
-             
-             con.close();
-         } catch (Exception e) {
-             System.out.println(e);
-         }
+         return positionIdLimit;
      }
      
      public void callLastDisplayMethod(int currRowSpan) {
@@ -217,6 +361,9 @@ public class Function extends DBConnection {
                  displaySuspendedEmployee(currRowSpan);
                  break;
              case 3:
+                 displayAllUnsuspendEmployeeInfo(currRowSpan);
+                 break;
+             case 4:
                  displayEmployeeByPosition(positionIdEntered,currRowSpan);
                  break;
          }
@@ -267,188 +414,36 @@ public class Function extends DBConnection {
          return false;
      }
      
-     //MY CHANGES BY: dperrr
-     public void displayAllEmployeeInfo() {
- 	    String query = "SELECT * FROM employees "
- 	                 + "INNER JOIN positions ON employees.position_id = positions.position_id "
- 	                 + "WHERE employee_is_suspended = 0";
+     //delete api's
+     
+     //API FOR DELETING EMPLOYEE
+    public boolean deleteEmployee(int employeeId) {
+        boolean isSuccess = false;
+        
+        if (!isEmployeeExists(employeeId)) {
+            System.out.println("Employee with ID " + employeeId + " does not exist.");
+            return isSuccess;
+        }
 
- 	    try {
- 	        connect();
- 	        state = con.createStatement();
- 	        result = state.executeQuery(query);
+        String query = "DELETE FROM employees "
+                        +"WHERE employee_id = ?" ;
 
- 	        System.out.printf("%-15s%-25s%-10s%-5s%-20s\n",
- 	                "EMPLOYEE ID", "Employee Name", "Gender", "Age", "Position Name");
+        try {
 
- 	        while (result.next()) {
- 	            int employeeId = result.getInt("employee_id");
- 	            String employeeName = result.getString("employee_name");
- 	            String gender = result.getString("employee_sex");
- 	            int employeeAge = result.getInt("employee_age");
- 	            String positionName = result.getString("position_name");
+                connect();
+                prep = con.prepareStatement(query);
+                prep.setInt(1, employeeId);
+                System.out.println("\nEmployee with  " + employeeId + " is sucessfully deleted...\n");
+                prep.executeUpdate();
+                con.close();
 
- 	            System.out.printf("[%d]\t\t%-25s%-10s%-5d%-20s\n",
- 	                    employeeId, employeeName, gender, employeeAge, positionName);
- 	        }
- 	    } catch (Exception e) {
- 	        System.out.println("Connection Error: " + e.getMessage());
- 	    }
- 	}
- 	
- 	public void displayEmployeePayroll() {
- 	    String query = "SELECT attendances.*, positions.position_pay_per_day, employees.employee_name "
- 	            + "FROM attendances "
- 	            + "INNER JOIN employees ON attendances.employee_id = employees.employee_id "
- 	            + "INNER JOIN positions ON positions.position_id = employees.position_id "
- 	            + "ORDER BY employee_id";
-
- 	    int netSalary = 0;
- 	    float grossSalary = 0;
- 	    int lateDeductions = 50;
- 	    System.out.println("********************************************************************");
- 	    System.out.println("*\t\t\t\t\t\t\t\t   *");
- 	    System.out.println("*\t\t     Payroll for the month of December\t           *");
- 	    System.out.println("*\t\t\t\t\t\t\t\t   *");
- 	    System.out.println("********************************************************************");
- 	    System.out.println("Employee ID\t  Employee Name\t\tNet Salary\tGross Salary");
-
- 	    try {
- 	        connect();
- 	        state = con.createStatement();
- 	        result = state.executeQuery(query);
-
- 	        while (result.next()) {
- 	            int noOfPresent = result.getInt("no_of_present");
- 	            int noOfAbsent = result.getInt("no_of_absent");
- 	            int noOfHoursLate = result.getInt("no_of_hours_late");
- 	            int payPerDay = result.getInt("position_pay_per_day");
- 	            int employeeId = result.getInt("employee_id");
- 	            String employeeName = result.getString("employee_name");
-
- 	            netSalary = payPerDay * noOfPresent;
- 	            grossSalary = netSalary - (noOfAbsent * payPerDay + (noOfHoursLate * lateDeductions));
-
- 	            System.out.printf("%-20d%-20s%-15d%-15.2f\n", employeeId, employeeName, netSalary, grossSalary);
- 	           
- 	        }
- 	    } catch (Exception e) {
- 	        e.printStackTrace();
- 	    }
- 	}
- 	
- 	//API FOR ADDING EMPLOYEE
- 	public void addEmployee(String employeeName, String gender, int employeeAge, int positionId, String password) {
- 	    String query = "INSERT INTO employees (employee_name, employee_sex, employee_age, position_id) VALUES (?, ?, ?, ?)";
- 	    
- 	    	
- 	    try {
- 	        connect();
- 	        prep = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-
- 	        prep.setString(1, employeeName);
- 	        prep.setString(2, gender);
- 	        prep.setInt(3, employeeAge);
- 	        prep.setInt(4, positionId);
- 	        prep.executeUpdate();
-
- 	        // Retrieve the auto increment employeeId
- 	        ResultSet generatedKeys = prep.getGeneratedKeys();
- 	        if (generatedKeys.next()) {
- 	            int generatedEmployeeId = generatedKeys.getInt(1);
- 	            addEmployeeCredentials(generatedEmployeeId, password);
- 	            addToAttendances(generatedEmployeeId);
- 	        }
-
- 	        con.close();
- 	        System.out.println("\n------------------------------");
- 	        System.out.println("Successfully added an employee");
- 	        System.out.println("------------------------------\n");
- 	        displayAllEmployeeInfo();
-
- 	    } catch (Exception e) {
- 	        System.out.println("Connection Error" + e.getMessage());
- 	    }
- 	}
-
- 	
- 	
- 	// METHOD FOR ADDING EMPLOYEE CREDENTIALS
- 	public void addEmployeeCredentials(int employeeId, String password) {
- 	    String query = "INSERT INTO users (user_password, employee_id) VALUES (?, ?)";
-
- 	    try {
- 	        connect();
- 	        prep = con.prepareStatement(query);
- 	        prep.setString(1, password);
- 	        prep.setInt(2, employeeId);
- 	        prep.executeUpdate();
- 	        con.close();
- 	    } catch (Exception e) {
- 	        e.printStackTrace();
- 	    }
- 	}
- 	
- 	
- 	public void addToAttendances(int employeeId) {
- 		 String attendanceQuery = "INSERT INTO attendances (employee_id) VALUES (?)";
- 		 
- 		 try {
- 			 connect();
- 			 prep = con.prepareStatement(attendanceQuery);
- 			 prep.setInt(1, employeeId);
- 			 prep.executeUpdate();
- 			 con.close();
- 		 }catch(Exception e) {
- 			 System.out.println(e);
- 		 }
- 	}
- 	
- 	
- 	//METHOD FOR CHECKING IF THE EMPLOYEE EXISTS IN OUR DATABASE
- 	private boolean isEmployeeExists(int employeeId) {
- 	    String checkQuery = "SELECT COUNT(*) FROM employees WHERE employee_id = ?";
- 	    try {
- 	        connect();
- 	        prep = con.prepareStatement(checkQuery);
- 	        prep.setInt(1, employeeId);
- 	        ResultSet result = prep.executeQuery();
- 	        result.next();
- 	        int count = result.getInt(1);
- 	        con.close();
- 	        return count > 0;
- 	    } catch (Exception e) {
- 	        e.printStackTrace();
- 	        return false; 
- 	    }
- 	}
- 	
- 	//API FOR DELETING EMPLOYEE
- 	public void deleteEmployee(int employeeId) {
- 		
-
- 		if (!isEmployeeExists(employeeId)) {
- 	        System.out.println("Employee with ID " + employeeId + " does not exist.");
- 	        return;
- 	    }
- 		String query = "DELETE FROM employees "
- 				+"WHERE employee_id = ?" ;
- 		
- 		try {
- 			
- 			connect();
- 			prep = con.prepareStatement(query);
- 			prep.setInt(1, employeeId);
- 			System.out.println("\nEmployee with  " + employeeId + " is sucessfully deleted...\n");
- 			prep.executeUpdate();
- 			con.close();
- 			
- 			displayAllEmployeeInfo();
- 			
- 			
- 		}catch(Exception e) {
- 			e.printStackTrace();
- 		}
- 		
- 	}
+                displayEmployeeBasicInfo(0);
+                    
+                isSuccess = true;
+        }catch(Exception e) {
+                e.printStackTrace();
+        }
+        
+        return isSuccess;
+    }
 }
