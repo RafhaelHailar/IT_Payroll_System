efd7efa81b7cb4a7b0edfc1f6b4865414bd8aff2
dperrr <rosellonjasperdan30@gmail.com>
2024-01-30 03:38:00 +0800

    ADDED A EMPLOYEE PAYROLL DETAILS

--- src/com/app/backend/Function.java
+++ src/com/app/backend/Function.java
@@ -12,6 +12,13 @@ public class Function extends DBConnection {
      private int positionIdEntered;
      public static boolean dataDisplaying = false;
      
+ 	 private final int SSS = 1000;
+     private final int MEDICARE = 500;
+
+     private float netSalary = 0;
+     private int grossSalary = 0;
+     private int noOfHoursLate;
+     
      // read api's
      public void display() {
          String query = "Select * FROM users";
@@ -301,50 +308,129 @@ public class Function extends DBConnection {
  	    }
  	}
      
- 	/**
- 	 * Displays the payroll information for employees for the specified month.
- 	 * The method retrieves attendance and employee details to calculate net and gross salaries.
+     /**
+ 	 * Displays the salary information for employees for the specified month.
  	 */
- 	public void displayEmployeePayroll() {
- 	    String query = "SELECT attendances.*, positions.position_pay_per_day, employees.employee_name "
- 	            + "FROM attendances "
- 	            + "INNER JOIN employees ON attendances.employee_id = employees.employee_id "
- 	            + "INNER JOIN positions ON positions.position_id = employees.position_id "
- 	            + "ORDER BY employee_id";
 
- 	    int netSalary = 0;
- 	    float grossSalary = 0;
- 	    int lateDeductions = 50;
- 	    System.out.println("********************************************************************");
- 	    System.out.println("*\t\t\t\t\t\t\t\t   *");
- 	    System.out.println("*\t\t     Payroll for the month of December\t           *");
- 	    System.out.println("*\t\t\t\t\t\t\t\t   *");
- 	    System.out.println("********************************************************************");
- 	    System.out.println("Employee ID\t  Employee Name\t\tNet Salary\tGross Salary");
+     public void displayEmployeeSalary() {
+         String query = "SELECT attendances.*, positions.position_pay_per_day, employees.employee_name "
+                 + "FROM attendances "
+                 + "INNER JOIN employees ON attendances.employee_id = employees.employee_id "
+                 + "INNER JOIN positions ON positions.position_id = employees.position_id "
+                 + "ORDER BY employee_id";
 
- 	    try {
- 	        connect();
- 	        state = con.createStatement();
- 	        result = state.executeQuery(query);
+         System.out.println("********************************************************************");
+         System.out.println("*\tEmployee Salary for the month of December\t           *");
+         System.out.println("********************************************************************");
+         System.out.println("Employee ID\t  Employee Name\t\tGross Salary\tNet Salary");
 
- 	        while (result.next()) {
- 	            int noOfPresent = result.getInt("no_of_present");
- 	            int noOfAbsent = result.getInt("no_of_absent");
- 	            int noOfHoursLate = result.getInt("no_of_hours_late");
- 	            int payPerDay = result.getInt("position_pay_per_day");
- 	            int employeeId = result.getInt("employee_id");
- 	            String employeeName = result.getString("employee_name");
+         try {
+             connect();
+             state = con.createStatement();
+             result = state.executeQuery(query);
 
- 	            netSalary = payPerDay * noOfPresent;
- 	            grossSalary = netSalary - (noOfAbsent * payPerDay + (noOfHoursLate * lateDeductions));
+             while (result.next()) {
+                 int noOfPresent = result.getInt("no_of_present");
+                 noOfHoursLate = result.getInt("no_of_hours_late"); 
+                 int payPerDay = result.getInt("position_pay_per_day");
+                 int employeeId = result.getInt("employee_id");
+                 String employeeName = result.getString("employee_name");
 
- 	            System.out.printf("%-20d%-20s%-15d%-15.2f\n", employeeId, employeeName, netSalary, grossSalary);
- 	           
- 	        }
- 	    } catch (Exception e) {
- 	        e.printStackTrace();
+                 calculateSalaries(noOfPresent, payPerDay);
+                 netSalary = getNetSalary();
+
+                 System.out.printf("%-20d%-20s%-15d%-15.2f\n", employeeId, employeeName, grossSalary, netSalary);
+             }
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
+     }
+
+     private void calculateSalaries(int noOfPresent, int payPerDay) {
+         grossSalary = payPerDay * noOfPresent;
+     }
+
+     private float getNetSalary() {
+         int lateDeductions = 50;
+         int tax = calculateTax(grossSalary);
+         return grossSalary - (tax + SSS+ MEDICARE + (noOfHoursLate * lateDeductions));
+     }
+
+     private int calculateTax(float grossSalary) {
+         if (grossSalary <= 20000) {
+             return 0;
+         } else if (grossSalary <= 30000) {
+             return 1000;
+         } else if (grossSalary <= 40000) {
+             return 2000;
+         } else {
+             return 3000;
+         }
+     }
+     
+     /**
+      * Displays employee payroll details based on the specific employeeId
+      * @param employeeId
+      */
+     public void displayEmployeePayroll(int employeeId) {
+     	
+     	String query = "SELECT attendances.*, positions.position_pay_per_day, positions.position_name, employees.employee_name "
+     	        + "FROM attendances "
+     	        + "INNER JOIN employees ON attendances.employee_id = employees.employee_id "
+     	        + "INNER JOIN positions ON positions.position_id = employees.position_id "
+     	        + "WHERE employees.employee_id = " + employeeId + " "
+     	        + "ORDER BY employees.employee_id";
+     	
+     	if (!isEmployeeExists(employeeId)) {
+ 	        System.out.println("Employee with ID " + employeeId + " does not exist.");
+ 	        return;
  	    }
- 	}
+     	
+     	
+         System.out.println("*************************************");
+         System.out.println("\t\tEmployee Payroll");
+         System.out.println("*************************************");
+
+         try {
+             connect();
+             state = con.createStatement();
+             result = state.executeQuery(query);
+
+             while (result.next()) {
+                 int noOfPresent = result.getInt("no_of_present");
+                 noOfHoursLate = result.getInt("no_of_hours_late");
+                 int payPerDay = result.getInt("position_pay_per_day");
+                 String employeeName = result.getString("employee_name");
+                 String positionName = result.getString("position_name");
+
+                 calculateSalaries(noOfPresent, payPerDay);
+                 netSalary = getNetSalary();
+
+                 displayPayrollDetails(employeeId, employeeName, positionName, payPerDay, noOfPresent);
+             }
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
+     }
+
+     private void displayPayrollDetails(int employeeId, String employeeName, String positionName, int payPerDay, int noOfPresent) {
+         System.out.println("EmployeeName: " + employeeName);
+         System.out.println("Employee ID: " + employeeId);
+         System.out.println("Position: " + positionName);
+         System.out.println("Salary Rate: " + payPerDay);
+         System.out.println("***********************************************\n");
+
+         System.out.println("Date Covered: December 1-30 2023");
+         System.out.println("Total No. of Days Present: " + noOfPresent);
+         System.out.println("Gross Salary: " + grossSalary);
+         System.out.println("Deductions: \n"
+                 + "\nTax: " + calculateTax(netSalary)
+                 + "\nSSS: " + SSS
+                 + "\nMedicare: " + MEDICARE);
+         System.out.println("***********************************************\n");
+         System.out.println("Net Salary: " + netSalary);
+     }
+
  	
 	/**
 	 * API for adding a new employee to the system.
@@ -384,7 +470,6 @@ public class Function extends DBConnection {
  	    }
  	}
 
- 	
  	
  	/**
 	 * Adds credentials for a new employee to the 'users' table.
